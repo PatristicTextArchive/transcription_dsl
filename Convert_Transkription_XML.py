87f57210c8b011f567d1fc95aa823a3294f822c8
Annette von Stockhausen <annette.von_stockhausen@bbaw.de>
2022-01-07 18:13:47 +0100

    Aktualisierung

--- Convert_Transkription_XML.py
+++ Convert_Transkription_XML.py
@@ -3,35 +3,40 @@
 
 # # Convert Transkription to XML
 
-# In[8]:
+# In[ ]:
 
 
 import os,glob,re
-data_dir = os.path.expanduser('PATH_TO_DIR/*.txt')
-data_paths = glob.glob(data_dir)
+# input needed here:
+data_dir = os.path.expanduser('~/Downloads') # sources
+xml_dir = os.path.expanduser('~/Downloads') # converted xml-files
+
+data_paths = glob.glob(data_dir+"/*.txt")
 
 for file in sorted(data_paths):
     print(file)
     content = open(file).read()
-    book = re.search(r'TEXT: (.+?)\n', content)
-    ms = re.search(r'HS: ([\w\s()\.]+?)\n', content)
+    text = re.search(r'TEXT: ([\w\s(),]+?)\n', content)
+    ms = re.search(r'HS: ([\w\s]+?)_([\w\s]+?)_([\w\s]+?)_([\w\s]+?)\n', content)
+    sigle = re.search(r'SIGLE: ([\w\s()\.]+?)\n', content)
+    diktyon = re.search(r'DIKTYON: ([0-9]+?)\n', content)
+    editor = re.search(r'EDITOR: ([\w\s]+?) \(([\w\s]+?)\)\n', content)
+    date = re.search(r'DATUM: ([0-9\-]+?)\n', content)
     initial_page = re.search(r'\(([0-9rv]+?)\)', content)
     #comment = re.search(r'Anmerkung: (.+?)\n', content)
     content_new = re.sub(r'TEXT: .+?\n', r'', content) # Löschen
     content_new = re.sub(r'HS: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'SIGLE: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'EDITOR: .+?\n', r'', content_new) # Löschen
     content_new = re.sub(r'PDF: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'online: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'SCAN: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'DIKTYON: .+?\n', r'', content_new) # Löschen
+    content_new = re.sub(r'DATUM: .+?\n', r'', content_new) # Löschen
     content_new = re.sub(r'\n\([0-9rv]+?\)', r'', content_new) # Löschen
     #content_new = re.sub(r'Anmerkung: .+?\n', r'', content_new) # Löschen
-    print(book.group(1),ms.group(1),initial_page.group(1))
-    with open("/PATH_TO_DIR/TEXT_ID-"+ms.group(1)+"-transcript.xml", "w") as f:    
-        # Umbrüche
-        #content_new = re.sub(r'\(([\w\d]+?)\)\n', r'', content_new) # Löschen
-        content_new = re.sub(r' ///\(([\w\d]+?)\)\n', r' <pb n="\1"/> ', content_new)
-        content_new = re.sub(r'///\(([\w\d]+?)\)\n', r'<pb n="\1" break="no"/>', content_new) # im Wort
-        content_new = re.sub(r' //', r' <cb/> ', content_new)
-        content_new = re.sub(r'//', r'<cb break="no"/>', content_new) # im Wort
-        content_new = re.sub(r' /[^>\w]{1}', r' <lb/> ', content_new)
-        content_new = re.sub(r'/[^>\w]{1}', r'<lb break="no"/>', content_new) # im Wort
+    print(text.group(1),sigle.group(1),initial_page.group(1))
+    with open(xml_dir+text.group(1)+".pta-Ms"+sigle.group(1)+".xml", "w") as f:    
         # Besondere Texte
         content_new = re.sub(r'3([\w\s]+?)~([1-4]{1})~3', r'<note place="margin_\2">\1</note>', content_new) # Marginalien; Nachbearbeitung bzgl. Positionsnummer:
         content_new = re.sub(r'<note place="margin_1">', r'<note place="top">', content_new)
@@ -49,62 +54,87 @@ for file in sorted(data_paths):
         content_new = re.sub(r'<del rend="1">', r'<del rend="erasure">', content_new)
         content_new = re.sub(r'<del rend="2">', r'<del rend="strikethrough">', content_new)
         content_new = re.sub(r'<del rend="3">', r'<del rend="expunction">', content_new)
-        content_new = re.sub(r'<del rend="4">', r'<del rend="without">', content_new)
+        content_new = re.sub(r'<del rend="4">', r'<del rend="unmarked">', content_new)
+        content_new = re.sub(r'<del rend="5">', r'<del rend="underline">', content_new)
         #content_new = re.sub(r'5([\w\s]+?)5', r'<add>\1</add>', content_new) # Text über der Zeile; Kurzschreibung # obsolete
         content_new = re.sub(r'5([\w\s]+?)~([1-3]{1})~5', r'<add place="\2">\1</add>', content_new) # Text über der Zeile
         content_new = re.sub(r'<add place="1">', r'<add place="above">', content_new) # auch für Textersetzung
         content_new = re.sub(r'<add place="2">', r'<add place="inline">', content_new)
         content_new = re.sub(r'<add place="3">', r'<add place="margin">', content_new)
-        content_new = re.sub(r'6...([0-9a-zA-Z]+?)...6', r'<gap reason="lost" quantity="\1" unit="character"/>', content_new) # Lücke im Text
+        content_new = re.sub(r'6...([0-9a-zA-Z]+?)...6', r'<gap reason="damage" quantity="\1" unit="character"/>', content_new) # Lücke im Text
         content_new = re.sub(r'7...([0-9a-zA-Z]+?)...7', r'<gap reason="illegible" quantity="\1" unit="character"/>', content_new) # Nicht lesbarer Text
         content_new = re.sub(r'8([\w\s@]+?)8', r'<unclear>\1</unclear>', content_new) # unsichere Lesung
         content_new = re.sub(r'9([\w]+?)~([\w]+?)9', r'<choice><expan>\2</expan><abbr type="nomSac"><hi rend="overline">\1</hi></abbr></choice>', content_new) # nomina sacra
         # Zitate
         content_new = re.sub(r'@@', r'<g type="doubled_diple"/>', content_new)
-        content_new = re.sub(r'@ ', r'<g type="diple"/>', content_new)
-        content_new = re.sub(r'— ', r'<g type="paragraphos"/>', content_new)
+        content_new = re.sub(r'@', r'<g type="diple"/>', content_new)
+        content_new = re.sub(r'[–]', r'<g type="paragraphos"/>', content_new)
+        # Zahlen
+        content_new = re.sub(r'10([\D]{1}?)10', r'<num>\1</num>', content_new)
         # Initialen und Ektheseis
-        content_new = re.sub(r'1([\D]+?)1', r'<hi rend="initial">\1</hi>', content_new)
-        content_new = re.sub(r'2([\D]+?)2', r'<hi rend="ekthesis">\1</hi>', content_new)
+        content_new = re.sub(r'1([\D]{1}?)1', r'<hi rend="initial">\1</hi>', content_new)
+        content_new = re.sub(r'2([\D]{1}?)2', r'<hi rend="ekthesis">\1</hi>', content_new)
+        # Umbrüche
+        #content_new = re.sub(r'\(([\w\d]+?)\)\n', r'', content_new) # Löschen
+        content_new = re.sub(r' ///\(([\w\d\.]+?)\) \n', r' <pb n="\1"/> ', content_new)
+        content_new = re.sub(r'///\(([\w\d]+?)\)\n', r'<pb n="\1" break="no"/>', content_new) # im Wort
+        content_new = re.sub(r' //', r' <cb/> ', content_new)
+        content_new = re.sub(r'//', r'<cb break="no"/>', content_new) # im Wort
+        content_new = re.sub(r' /[^>\w]{1}', r' <lb/> ', content_new)
+        content_new = re.sub(r'/[^>\w]{1}', r'<lb break="no"/>', content_new) # im Wort
         # Titel und Lemma
         title = re.search(r'(?<!\d)0(.+?)(?<!\d)0', content_new, flags=re.DOTALL)
         content_new = re.sub(r'(?<!\d)0(.+?)(?<!\d)0', r'', content_new, flags=re.DOTALL) # Löschen
         ## Ausdruck
        # XML-Grundgerüst
         print('''<?xml version="1.0" encoding="UTF-8"?>
+<?xml-model href="https://raw.githubusercontent.com/PatristicTextArchive/Schema/master/tei-pta.rng" schematypens="http://relaxng.org/ns/structure/1.0"?>
 <TEI xmlns="http://www.tei-c.org/ns/1.0">
     <teiHeader>
         <fileDesc>
             <titleStmt>
-                <title xml:lang="la">'''+book.group(1)+'''</title>
-                <author>Theodoret</author>
+                <title xml:lang="la">'''+text.group(1)+'''</title>
+                <author>XXX</author>
+                <respStmt>
+                    <resp>transkribiert von</resp>
+                    <persName xml:id="'''+editor.group(2)+'''">'''+editor.group(1)+'''</persName>
+                </respStmt>
             </titleStmt>
+            <editionStmt>
+                <p>New transcription.</p>
+            </editionStmt>
             <publicationStmt>
-                <publisher>BBAW Bibelexegese</publisher>
-                <idno type="filename">CPG6207-'''+ms.group(1)+'''-transcript.xml</idno>
+                <publisher>Patristic Text Archive</publisher>
+                <date>'''+date.group(1)+'''</date>
+                <idno type="PTA">'''+text.group(1)+'''</idno>
             </publicationStmt>
             <sourceDesc>
                 <msDesc>
-                    <msIdentifier>
-                        <settlement></settlement>
-                        <repository></repository>
-                        <idno>'''+ms.group(1)+'''</idno>
+                    <msIdentifier xml:id="'''+sigle.group(1)+'''">
+                        <settlement>'''+ms.group(1)+'''</settlement>
+                        <repository>'''+ms.group(2)+'''</repository>
+                        <idno>'''+ms.group(3)+''' '''+ms.group(4)+'''</idno>
                         <altIdentifier type="diktyon">
-                            <idno></idno>
+                            <idno>'''+diktyon.group(1)+'''</idno>
                         </altIdentifier>
                     </msIdentifier>
                 </msDesc>'''
           #    <note>'''+comment.group(1)+'''</note>
             '''</sourceDesc>
         </fileDesc>
+        <encodingDesc>
+            <refsDecl n="CTS">
+                <cRefPattern n="chapter" matchPattern="(.+)" replacementPattern="#xpath(/tei:TEI/tei:text/tei:body/tei:div[@type='edition']/tei:div[@n='$1'])"/>
+            </refsDecl>
+        </encodingDesc>
         <revisionDesc>
-            <change who="#SH" when="2020-01-21">Transkript der Handschrift.</change>
+            <change who="#'''+editor.group(1)+'''" when="'''+date.group(1)+'''">Transkript der Handschrift.</change>
         </revisionDesc>
     </teiHeader>
     <text xml:lang="grc">
         <body>
-            <div type="edition">
-            <pb n="'''+initial_page.group(1)+'''"/><head><title>'''+title.group(1)+'''</title></head>
+            <div type="edition"><pb n="'''+initial_page.group(1)+'''"/>
+            <head><title>'''+title.group(1)+'''</title></head>
             <div type="textpart" subtype="chapter" n="1">
             <p>'''+content_new+'''</p>
                 </div>
@@ -122,3 +152,7 @@ for file in sorted(data_paths):
 
 
 # In[ ]:
+
+
+
+
--- Transkriptionsrichtlinien-Abkürzungen.md
+++ Transkriptionsrichtlinien-Abkürzungen.md
@@ -6,14 +6,24 @@ output: word_document
 Am Beginn jeder Datei wird notiert:
 
 ```
-TEXT: Autor Titel Nummer des Buches
-HS: Sigle (Handschrift)
+TEXT: PTA-ID (sc. pta0001.pta001)
+HS: Ort_Bibliothek_Sammlung_Nr
+SIGLE: Sigle
+DIKTYON: Diktyon-ID
+EDITOR: Name (Kürzel)
+DATUM: Datum der Transkription im Format 2022-01-01
+```
+
+Weitere, mögliche Angaben im Kopf der Transkription (werden vom Script nicht verwendet):
+
+```
 PDF: S. Seitenzahl in PDF
+SCAN: URL
 ```
 
 # Titel/Lemma
 
-Wird mit `0Lemma/Titel0` ausgezeichnet.
+Wird mit `0Lemma/Titel0` ausgezeichnet. **Muss** angegeben werden, ggfs. leer lassen (sc. `0 0`)
 
 # Umbrüche
 
@@ -22,6 +32,8 @@ Umbrüche werden jeweils am Ende der Zeile angegeben.
 - Zeilenumbruch: `/` (Griechische Tastatur: -Leerzeichen) (Leerzeichen davor und danach, im Wort ohne Leerzeichen davor oder danach)
 - Spaltenumbruch: `//` (Leerzeichen davor und danach, im Wort ohne Leerzeichen davor oder danach)
 - Seitenumbruch: `///(Folioangabe)` (Leerzeichen davor und danach, im Wort ohne Leerzeichen davor oder danach)
+
+Die erste Seite der Transkription wird mit `(Folionr+r/v)` am Beginn der Transkription angegeben (sc. `(112r)`).
 
 # Zeichensetzung
 
@@ -40,7 +52,7 @@ Absätze werden durch eine leere Zeile markiert.
 
 - Marginalien: `3Text~Position:1=oben/2=unten/3=innen/4=außen~3` (~: Griechische Tastatur = ^Leerzeichen)
 - getilgter Text: `4TextTextText~1=Rasur/2=durchgestrichen/3=expungiert~4`
-- Korrektur: `4ersetzter Text~0=Verweiszeichen/1=Rasur/2=durchgestrichen/3=expungiert/4=ohne Zeichen~korrigierender Text~Position:1=über der Zeile/2=in der Zeile/3=Rand~4`
+- Korrektur: `4ersetzter Text~0=Verweiszeichen/1=Rasur/2=durchgestrichen/3=expungiert/4=ohne Zeichen/5=unterstrichen~korrigierender Text~Position:1=über der Zeile/2=in der Zeile/3=Rand~4`
 - hinzugefügter Text (ohne Korrektur): `5hinzugefügter Text~Position:1=über der Zeile/2=in der Zeile/3=Rand~5`
 - Lücke im Text: `6...geschätzte Anzahl der Buchstaben...6`
 - Nicht lesbarer Text: `7...Anzahl der Buchstaben...7`
@@ -50,7 +62,7 @@ Absätze werden durch eine leere Zeile markiert.
 
 # Zitate
 
-In der Handschrift mit Diple markierte Zitate werden durch `@ ` (Griechische Tastatur: : ) *und folgendem Leerzeichen* am Beginn der jeweiligen Zeile markiert.
+In der Handschrift mit Diple markierte Zitate werden durch `@ `, ggfs. doppelt, (Griechische Tastatur: `:`) *und folgendem Leerzeichen* am Beginn der jeweiligen Zeile markiert.
 
 # Paragraphos
 
